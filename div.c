135bd1d851b250feecb7c0d9c79c2a38ec335b55
mayanksoni052 <35027941+mayanksoni052@users.noreply.github.com>
2020-10-01 23:30:54 +0530

    Added comments for make code clean

--- div.c
+++ div.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
 	int a, b;
-	printf("Enter two numbers: ");
+	printf("Enter two numbers: "); // For user input
 	scanf("%d %d", &a, &b);
-	printf("%d", a/b);
+	printf("%d", a/b); // Do division here
 	return 0;
 }
